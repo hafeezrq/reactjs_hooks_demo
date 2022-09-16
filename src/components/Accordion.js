aaa785d18bb46be8d3c3306ee48138b6b3fa7d30
hafeezrq <hafeezrq@yahoo.com>
2022-09-16 14:54:34 +1000

    Use useState() hook.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map(item => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = index => {
+    setActiveIndex(index);
+    console.log(activeIndex);
+  };
+  const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
-        <div className='title active'>
+        <div className='title active' onClick={() => onTitleClick(index)}>
           <i className='dropdown icon'></i>
           {item.title}
         </div>
