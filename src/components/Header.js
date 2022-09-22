c0aafe9915ab9336fe771930ea87c51e73ecdc2c
hafeezrq <hafeezrq@yahoo.com>
2022-09-22 19:09:31 +1000

    Make use of browser's window's event to make routes realise change in path and show/hide accordingly.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,21 @@
 import React from 'react';
+import Link from './Link';
 
 const Header = () => {
   return (
-    <div>
-      <div className='ui secondary pointing menu'>
-        <a href='/' className='item'>
-          Accordion
-        </a>
-        <a href='/list' className='item'>
-          Search
-        </a>
-        <a href='/dropdown' className='item'>
-          Dropdown
-        </a>
-        <a href='/translate' className='item'>
-          Translate
-        </a>
-      </div>
+    <div className='ui secondary pointing menu'>
+      <Link href='/' className='item'>
+        Accordion
+      </Link>
+      <Link href='/list' className='item'>
+        Search
+      </Link>
+      <Link href='/dropdown' className='item'>
+        Dropdown
+      </Link>
+      <Link href='/translate' className='item'>
+        Translate
+      </Link>
     </div>
   );
 };
